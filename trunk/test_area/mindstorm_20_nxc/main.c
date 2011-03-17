09711156772a7268ea7ae7d37f933cec45665f8a
flaming.zelda@gmail.com <flaming.zelda@gmail.com@4324a9da-489b-ea18-4a0b-e0fe39f471b6>
2011-03-17 01:35:41 +0000

    Trying that again...

---
+++ trunk/test_area/mindstorm_20_nxc/main.c
@@ -0,0 +1,28 @@
+/*
+  @file main.c
+  @auth Gary Steelman
+  @desc The main controller for the NXT 2.0
+  @edit 16 Mar 2011
+*/
+
+#include "_3x3Matrix.h"
+#include "_3DPoint.h"
+#include "_3DFrame.h"
+
+#include "cmath.h"
+#include "stdio.h"
+#include "cstring.h"
+
+task main()
+{
+  printf( "lol\n" );
+  
+  _3x3Matrix M;
+  
+  _3DFrame F;
+  
+  _3DPoint P;
+  
+  
+  
+}
