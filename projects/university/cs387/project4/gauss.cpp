91a27f7bc12c2d29e7d7ad6e442945e2cdd483bd
flaming.zelda@gmail.com <flaming.zelda@gmail.com@4324a9da-489b-ea18-4a0b-e0fe39f471b6>
2011-04-09 10:06:45 +0000

    Upload: -cs345 semester project beginnings. --main.nxc - The main routine for the robot. --_3DFrame.h - Contains the 3D tracking matrices for the different robot joints --numeric_constants.h - Copy of the lib file because BricxCC doesn't allow relative include paths.
    Update:
    -cs387/project4/gauss.cpp - Almost working. Each of the steps is there, but there's a logic error (resulting in a segmentation fault) somewhere.

--- projects/university/cs387/project4/gauss.cpp
+++ projects/university/cs387/project4/gauss.cpp
@@ -21,7 +21,7 @@
 #include <mpi.h>
 
 // Global constants for readability and maintainability
-#define _MASTER_ID    0    // Master ID
+#define _MASTER_ID 0 // Master ID
 
 using namespace std;
 
@@ -29,6 +29,7 @@ using namespace std;
   PRE-DECLARATIONS
 ***/
 void driver( int argc, char* argv[] );
+bool myCompare( double i, double j );
 
 /***
   MAIN
@@ -37,6 +38,8 @@ int main( int argc, char * argv[] )
 {
   driver( argc, argv );
 
+  MPI_Finalize();
+  
   return 0;
 }
 
@@ -72,11 +75,11 @@ void driver( int argc, char* argv[] )
   string         fileLoc        = argv[1]; // The location of the input file
   string         currLine       = "";      // The current line of the input file
   int            dimension      = 0;       // The dimensionality of the nxn matrix
-  // int            numCols        = 0;       // The number of columns for this process
   int            extraCols      = 0;       // The number of extra columns that must
                                            // be appended to the matrix to make it square
   vector< vector<double> > myNumbers;      // Container for the numbers for this process
                                            // [numCols][dimension]
+  double         determinant    = 0;       // The determinant of the matrix
 
   double         wallClockStart = 0;       // Starting time of the algorithm
   double         wallClockEnd   = 0;       // Ending time of the algorithm
@@ -109,7 +112,6 @@ void driver( int argc, char* argv[] )
     if ( myID == _MASTER_ID )
       cout << "Error opening the input file \"" << fileLoc << endl;
 
-    MPI_Finalize();
     return;
   }
   
@@ -143,14 +145,42 @@ void driver( int argc, char* argv[] )
   inFile.close();
   
   // This process got shorted a column because the number of columns isn't even
+  // multiple of the number of processes and needs an identity column
   if ( myNumbers.size() < ceil( dimension / static_cast<float>(numProcs) ) )
   {
+    cout << myID << " appending an identity column: " << endl;
     app_col.clear();
     app_col.resize( dimension + extraCols, 0.0 );
-    app_col[dimension + myID - 1] = 1.0;
+    app_col[dimension + myID - 2] = 1.0;
     myNumbers.push_back( app_col );
+    cout << "  zero placed in index " << dimension + myID - 1 << endl;
   }
 
+  // Output the numbers for visual confirmation of correctness
+  /* for ( int i = 0; i < myNumbers.size(); i++ )
+  {
+    cout << myID << "," << i << ": ";
+    
+    for ( int j = 0; j < myNumbers[i].size(); j++ )
+    {
+      cout << myNumbers[i][j] << " ";
+    }
+    
+    cout << endl;
+  } */  
+  
+  cout << myID << " has " << myNumbers.size() << " columns." << endl;
+  
+  if ( myID == numProcs - 1 )
+  {
+    cout << "Processor << " <<  myID << "," << myNumbers.size()-1 << ": ";
+    
+    for ( int q = 0; q < myNumbers[0].size(); q++ )
+      cout << myNumbers[myNumbers.size()-1][q] << " ";
+      
+    cout << endl;
+  }
+  
   // Ensure all processes have finished reading input before continuing
   MPI_Barrier( MPI_COMM_WORLD );
 
@@ -164,20 +194,117 @@ void driver( int argc, char* argv[] )
     wallClockStart = MPI_Wtime();
   }
   
+  MPI_Barrier( MPI_COMM_WORLD );
+  
+  int    local_max_idx = 0;
+  double local_det     = 1.0;
+  int    numSwaps      = 0;
+  int    my_i          = 0;
+  vector<double> mult_factors;  
+  
+  // For each column in the matrix
+  for ( int i = 0; i < (dimension + extraCols); i++ )
+  {
+    // This process performs operations on its my_ith column relative to the 
+    // iteration over the entire matrix
+    my_i = i / numProcs;
+    
+    cout << myID << ": " << i << " -> " << my_i << " (pivot)" << endl;
+    
   /***
     Perform pivoting to avoid divide by zero and error propogation
-  ***/
+  ***/  
+  
+    // This process owns this column
+    if ( i % numProcs == myID )
+    {
+      /* local_max_idx = distance
+                      ( 
+                        myNumbers[my_i].begin(), 
+                        max_element
+                        ( 
+                          myNumbers[my_i].begin()+i,
+                          myNumbers[my_i].end(),
+                          myCompare
+                        ) 
+                      // );
+                      ) - 1; //?!?! */
+                      
+      local_max_idx = i;
+      for ( int k = i; k < myNumbers[my_i].size(); k++ )
+        if ( myCompare( myNumbers[my_i][local_max_idx], myNumbers[my_i][k] ) )
+          local_max_idx = k;                      
 
+      // Multiply the local determinant by the value about to be placed on the 
+      // global diagonal
+      local_det *= myNumbers[my_i][local_max_idx];
+    }
+    
+    // Broadcast the location of the max value found to all processes
+    MPI_Bcast( &local_max_idx, 1, MPI_DOUBLE, (i % numProcs), MPI_COMM_WORLD );
+    
+    // Swap the value from the diagonal, increment counter
+    if ( i != local_max_idx )
+    {
+      swap( myNumbers[my_i][i], myNumbers[my_i][local_max_idx] );    
+      numSwaps++;
+    }
+   
   /***
-    Perform elimination to reduce matrix to upper triangular form
+    Calculate multiplication factors for row reduction
   ***/
+    cout << myID << ": " << i << " -> " << my_i << " (mult factors)" << endl;
+    // mult_factors.resize( dimension + extraCols - i );
+    mult_factors.resize( dimension + extraCols - i - 1 );
+    
+    cout << myID << ": " << i << " -> " << my_i << ": mult_factors.size() = " << mult_factors.size() << endl;
+    if ( mult_factors.size() > 0 )
+    {
+      if ( i % numProcs == myID )
+      {     
+        // Populate vector of multiplication factors to zero out this column
+        for ( int j = 0; j < mult_factors.size(); j++ )
+        {
+          mult_factors[j] = 
+            (myNumbers[my_i][i] == 0.0) ? 
+            0.0 : 
+            myNumbers[my_i][i+1+j] / myNumbers[my_i][i]
+          ;
+        }
+      }
+      
+      // Broadcast the multiplication factors to every process for row operation
+      MPI_Bcast( &mult_factors[0], mult_factors.size(), MPI_DOUBLE, (i % numProcs ), MPI_COMM_WORLD );
+    }
 
+    cout << myID << ": " << i << " -> " << my_i << " (row ops)" << endl;
+    
+    /***
+      Reduce row values based on multiplication factors
+    ***/
+    
+    // All processes perform row operation on their columns
+    for ( int k = 0; k < myNumbers.size(); k++ )
+    {
+      for ( int j = 0; j < mult_factors.size(); j++ )
+      {
+        // myNumbers[k][i+1+j] -= mult_factors[j] * myNumbers[my_i][i];
+        myNumbers[k][i+1+j] -= mult_factors[j] * myNumbers[k][i];
+        
+      }
+    }
+    
+    cout << myID << ": " << i << " -> " << my_i << " (end)" << endl;
+  }  
+  
   /***
     Calculate determinant of the resultant matrix
   ***/
-
-  // Wait for all processes to finish bitonic sorting
-  MPI_Barrier( MPI_COMM_WORLD );
+  
+  // Reduce w/ multiply across all processes' local determinant factors to obtain
+  // global determinant
+  MPI_Reduce( &local_det, &determinant, 1, MPI_DOUBLE, MPI_PROD, _MASTER_ID, MPI_COMM_WORLD );
+  determinant = (numSwaps & 1) ? (determinant) : (-1 * determinant);
 
   // Stop clock
   if ( myID == _MASTER_ID )
@@ -188,10 +315,13 @@ void driver( int argc, char* argv[] )
     cout << (wallClockEnd - wallClockStart) << " seconds, eqivalent to: \n"
          << (wallClockEnd - wallClockStart) / 60.0 << " minutes."
          << endl;
+    cout << "The computed determinant of the orignal matrix is: " << determinant << endl;
   }
 
+  // MPI_Barrier( MPI_COMM_WORLD );
+  
   // Output the numbers for visual confirmation of correctness
-  for ( int i = 0; i < myNumbers.size(); i++ )
+  /*   for ( int i = 0; i < myNumbers.size(); i++ )
   {
     cout << myID << "," << i << ": ";
     
@@ -201,14 +331,14 @@ void driver( int argc, char* argv[] )
     }
     
     cout << endl;
-  }
-  
-  // Output the calculated determinant 
-
-  // Finish the parallel computation
-  MPI_Finalize();
+  } */
 
   return;
+}
+
+inline bool myCompare( double i, double j )
+{
+  return (fabs( i ) < fabs ( j ));
 }
 
 /* END OF FILE */
