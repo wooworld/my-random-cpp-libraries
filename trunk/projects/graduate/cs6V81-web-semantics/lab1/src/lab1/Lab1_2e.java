d2011018086e119c5fb66186274f1263f3dc6e2d
flaming.zelda@gmail.com <flaming.zelda@gmail.com@4324a9da-489b-ea18-4a0b-e0fe39f471b6>
2011-09-28 21:02:41 +0000

    Update: -Refactored print statements in ai/homework2.java and lab1/Lab1_2e.java -Brought lab1 code up to Java coding standards.

--- trunk/projects/graduate/cs6V81-web-semantics/lab1/src/lab1/Lab1_2e.java
+++ trunk/projects/graduate/cs6V81-web-semantics/lab1/src/lab1/Lab1_2e.java
@@ -1,131 +1,104 @@
+/*
+ * Lab1_2e creates multiple Jena Model instances with various ways of populating
+ * the data inside the model.
+ *  
+ * @version
+ * 1.3 9/28/2011
+ * 
+ * @author
+ * Gary Steelman
+ */
+
 package lab1;
 
 import com.hp.hpl.jena.rdf.model.*; 
 import com.hp.hpl.jena.sdb.SDBFactory;
 import com.hp.hpl.jena.sdb.Store;
-import com.hp.hpl.jena.sdb.StoreDesc;
-import com.hp.hpl.jena.sdb.sql.JDBC;
-import com.hp.hpl.jena.sdb.sql.SDBConnection;
-import com.hp.hpl.jena.sdb.store.DatabaseType;
-import com.hp.hpl.jena.sdb.store.LayoutType;
-import com.hp.hpl.jena.sdb.store.StoreFactory;
 import com.hp.hpl.jena.vocabulary.*;
-import java.sql.*;
-
-
 import java.io.*;
 
-public class Lab1_2e 
-{
-	public static void main(String[] args) 
-	{		
-		String nsUTD = "http://utdallas/semclass#";
+/*
+ * Lab1_2e creates multiple Jena Model instances with various ways of populating
+ * the data inside the model.
+ */
+public class Lab1_2e {
+	public static void main(String[] args) {		
+		// Local variables for populating the Models
+		String nsUTD    = "http://utdallas/semclass#";
 		String seidaURI = nsUTD + "stevenSeida";
 		String fullName = "Steven Seida";
-		String fName = "Steven";
-		String lName = "Seida";
+		String fName    = "Steven";
+		String lName    = "Seida";
 		String birthday = "January 1, 1901";
-		String email = "steven.seida@utdallas.edu";
-		String title = "Senior Lecturer";
+		String email    = "steven.seida@utdallas.edu";
+		String title    = "Senior Lecturer";
 		
-		Model model = ModelFactory.createDefaultModel();	
+		//-------------------------------
+		// PART 1
+		//-------------------------------
+		Model inMemoryModel = ModelFactory.createDefaultModel();			
+		inMemoryModel.setNsPrefix( "nsUTD", nsUTD );
 		
-		model.setNsPrefix( "nsUTD", nsUTD );
-		
-		model.createResource( seidaURI ) 
+		// Populate the in-memory Model with the proper data
+		inMemoryModel.createResource( seidaURI ) 
 			.addProperty(VCARD.FN, fullName)
 			.addProperty(VCARD.EMAIL, email)
 			.addProperty(VCARD.BDAY, birthday )
 			.addProperty(VCARD.TITLE, title)
 			.addProperty(VCARD.N,
-					model.createResource()
+					inMemoryModel.createResource()
 						.addProperty(VCARD.Given, fName)
 						.addProperty(VCARD.Family, lName)
 					);
-		
-		try 
-		{
-			BufferedWriter outXML = new BufferedWriter( new FileWriter( "Lab1_1_gSteelman.xml" ) );
-			BufferedWriter outXMLClean = new BufferedWriter( new FileWriter( "Lab1_1_gSteelman-Clean.xml" ) );
-			BufferedWriter outN_TRIPLE = new BufferedWriter( new FileWriter( "Lab1_1_gSteelman.ntp" ) );
-			BufferedWriter outN3 = new BufferedWriter( new FileWriter( "Lab1_1_gSteelman.n3" ) );			
-			
-			model.write( outXML, "RDF/XML");
-			model.write( outXMLClean, "RDF/XML-ABBREV");
-			model.write( outN_TRIPLE, "N-TRIPLE");
-			model.write( outN3, "N3");			
-			
-			outXML.close();
-			outXMLClean.close();
-			outN_TRIPLE.close();
-			outN3.close();			
-		} 
-		
-		catch (IOException e) 
-		{
-			System.out.println( "Exception: " );
-			System.out.println( e.toString( ) );
-		}		
+		/*
+		 * Commented out because the instructions request only the named SDB
+		 * model's output.
+		 */
+		//print_results("1", inMemoryModel);
 		
 		//-------------------------------
 		// PART 2-1
 		//-------------------------------
 		System.setProperty("jena.db.user", "jenasdb");
 		System.setProperty("jena.db.password", "fastdb");
-			
+		
+		// Connect to the SDB database using an unnamed Model
 		Store store = SDBFactory.connectStore("sdb.ttl");
-		Model modelFromSDB = SDBFactory.connectDefaultModel(store);
+		Model unnamedSDBModel = SDBFactory.connectDefaultModel(store);
 		
-		modelFromSDB.removeAll();		
+		// Clear the database of all previous information
+		unnamedSDBModel.removeAll();		
+		unnamedSDBModel.setNsPrefix( "nsUTD", nsUTD);
 		
-		modelFromSDB.setNsPrefix( "nsUTD", nsUTD);
-		
-		modelFromSDB.createResource( seidaURI ) 
+		unnamedSDBModel.createResource( seidaURI ) 
 			.addProperty(VCARD.FN, fullName)
 			.addProperty(VCARD.EMAIL, email)
 			.addProperty(VCARD.BDAY, birthday )
 			.addProperty(VCARD.TITLE, title)
 			.addProperty(VCARD.N,
-					modelFromSDB.createResource()
+					unnamedSDBModel.createResource()
 						.addProperty(VCARD.Given, fName)
 						.addProperty(VCARD.Family, lName)
 					);
 
-		try 
-		{
-			BufferedWriter outXML = new BufferedWriter( new FileWriter( "Lab1_2_gSteelman.xml" ) );
-			BufferedWriter outXMLClean = new BufferedWriter( new FileWriter( "Lab1_2_gSteelman-Clean.xml" ) );
-			BufferedWriter outN_TRIPLE = new BufferedWriter( new FileWriter( "Lab1_2_gSteelman.ntp" ) );
-			BufferedWriter outN3 = new BufferedWriter( new FileWriter( "Lab1_2_gSteelman.n3" ) );			
-			
-			modelFromSDB.write( outXML, "RDF/XML");
-			modelFromSDB.write( outXMLClean, "RDF/XML-ABBREV");
-			modelFromSDB.write( outN_TRIPLE, "N-TRIPLE");
-			modelFromSDB.write( outN3, "N3");			
-			
-			outXML.close();
-			outXMLClean.close();
-			outN_TRIPLE.close();
-			outN3.close();			
-		} 
+		/*
+		 * Commented out because the instructions request only the named SDB
+		 * model's output.
+		 */
+		//print_results("2", unnamedSDBModel);
 		
-		catch (IOException e) 
-		{
-			System.out.println( "Exception: " );
-			System.out.println( e.toString( ) );
-		}
-		
-		modelFromSDB.close();	
+		unnamedSDBModel.close();	
 		
 		//-------------------------------
 		// PART 2-2
-		//-------------------------------				
+		//-------------------------------	
+		// Connect to the SDB database using a named Model
 		ModelMaker modelMaker = ModelFactory.createMemModelMaker();		
 		modelMaker.createModel("myrdf");
 		Model namedSDBModel = SDBFactory.connectNamedModel(store, "myrdf");
 		
-		namedSDBModel.removeAll();		
-		
+		// Clear the database of all previous information
+		namedSDBModel.removeAll();	
 		namedSDBModel.setNsPrefix( "nsUTD", nsUTD);
 		
 		namedSDBModel.createResource( seidaURI ) 
@@ -138,31 +111,54 @@ public class Lab1_2e
 						.addProperty(VCARD.Given, fName)
 						.addProperty(VCARD.Family, lName)
 					);
-
-		try 
-		{
-			BufferedWriter outXML = new BufferedWriter( new FileWriter( "Lab1_2-2_gSteelman.xml" ) );
-			BufferedWriter outXMLClean = new BufferedWriter( new FileWriter( "Lab1_2-2_gSteelman-Clean.xml" ) );
-			BufferedWriter outN_TRIPLE = new BufferedWriter( new FileWriter( "Lab1_2-2_gSteelman.ntp" ) );
-			BufferedWriter outN3 = new BufferedWriter( new FileWriter( "Lab1_2-2_gSteelman.n3" ) );			
-			
-			namedSDBModel.write( outXML, "RDF/XML");
-			namedSDBModel.write( outXMLClean, "RDF/XML-ABBREV");
-			namedSDBModel.write( outN_TRIPLE, "N-TRIPLE");
-			namedSDBModel.write( outN3, "N3");			
-			
-			outXML.close();
-			outXMLClean.close();
-			outN_TRIPLE.close();
-			outN3.close();			
-		} 
 		
-		catch (IOException e) 
-		{
+		print_results("2", namedSDBModel);
+		
+		namedSDBModel.close();			
+	}
+	
+	/*
+	 * Prints the contents of model to the output files for this program.
+	 * 
+	 * @param number -- The section of the lab being printed 
+	 * 
+	 * @param model -- The model to output the contents of
+	 * 
+	 * @see Model
+	 */
+	static void print_results(String number, Model model) {
+		try {
+			BufferedWriter outXML = new BufferedWriter( 
+					new FileWriter( "Lab1_" + number + "_gSteelman.xml" ) 
+					);
+			model.write( outXML, "RDF/XML");
+			outXML.close();
+			
+			
+			// Commented out because the instructions do not request this file format
+			/*
+			BufferedWriter outXMLClean = new BufferedWriter( 
+					new FileWriter( "Lab1_" + number + "_gSteelman-Clean.xml" ) 
+					);
+			model.write( outXMLClean, "RDF/XML-ABBREV");
+			outXMLClean.close();
+			*/			
+			
+			BufferedWriter outN_TRIPLE = new BufferedWriter( 
+					new FileWriter( "Lab1_" + number + "_gSteelman.ntp" ) 
+					);
+			model.write( outN_TRIPLE, "N-TRIPLE");
+			outN_TRIPLE.close();
+			
+			BufferedWriter outN3 = new BufferedWriter( 
+					new FileWriter( "Lab1_" + number + "_gSteelman.n3" ) 
+					);			
+			model.write( outN3, "N3");			
+			outN3.close();			
+			
+		} catch (IOException e) {			
 			System.out.println( "Exception: " );
 			System.out.println( e.toString( ) );
 		}
-		
-		namedSDBModel.close();			
 	}
 }
