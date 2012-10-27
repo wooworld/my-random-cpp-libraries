ec14455fdcba9aba3266702afa4f89c7067c8119
Flaming.Zelda@gmail.com <Flaming.Zelda@gmail.com@4324a9da-489b-ea18-4a0b-e0fe39f471b6>
2012-10-27 03:37:08 +0000

    Upload: -Very small skeleton for the quadcopter project for the UTD Robotics and Automation Society. This is a WIP and will be updated frequently. I hope.

---
+++ projects/graduate/UTDRAS/quadcopter/main.cpp
@@ -0,0 +1,25 @@
+#include "Controller.h" // TODO
+
+const unsigned int PATH_LENGTH = 1; 
+Matrix references[PATH_LENGTH]; // TODO
+Controller controller;
+
+setup() {
+  // Seed references with position 5 inches up
+}
+
+main() {
+  controller.start();
+
+  // While we haven't sent a kill signal to the controller
+  while ( controller.get_run_state() == CONT_RUN ) {
+    // For each reference make the controller transition to that reference state
+    for ( unsigned int i = 0; i < PATH_LENGTH; i++ ) {
+      controller.apply( references[i] );
+    }
+  }
+  
+  if ( controller.get_run_state() == CONT_ERROR ) {
+    exit(1);
+  }
+}
